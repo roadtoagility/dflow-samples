419654953e4ec6cff463837678c08fda9a2eab6c
drr00t <adribeiro@gmail.com>
2022-09-16 17:43:38 -0300

    missed file renaming

---
+++ Ecommerce/Ecommerce/Persistence/EcommerceAppDbContext.cs
@@ -0,0 +1,27 @@
+// Copyright (C) 2022  Road to Agility
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+using Ecommerce.Persistence.Mappings;
+using Ecommerce.Persistence.State;
+using Microsoft.EntityFrameworkCore;
+using TodoManagerApp.Persistence;
+
+namespace Ecommerce.Persistence;
+
+public sealed class EcommerceAppDbContext: SoftDeleteDbContext
+{
+    public EcommerceAppDbContext(DbContextOptions<EcommerceAppDbContext> contextOptions)
+        : base(contextOptions)
+    {
+        this.Database.EnsureCreated();
+        this.Database.EnsureDeleted();
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        new ProductStateMapping().Configure(modelBuilder.Entity<ProductState>());
+    }
+}
